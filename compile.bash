ee46c51823011f06a377d60bd648185ee7a53e10
Stuart Olsen <so1132@txstate.edu>
2013-04-19 01:06:03 -0500

    Getting somewhere

--- compile.bash
+++ compile.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 g++ -std=c++0x -O3 -I./ *.cc -c -I /usr/local/cuda/include/
-nvcc --compiler-options=-std=c++0x -O3 -arch=sm_20 -I./  cuda_wrapper.cu -c
+nvcc -O3 -arch=sm_20 -I./  cuda_wrapper.cu -c
+g++ *.o -o mandelbrot -L /usr/local/cuda/lib64/ -lcuda -lcudart
